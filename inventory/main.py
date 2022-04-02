d8c864f47295b863efb4813248309403482b1e19
kyong-dev <kyong.dev@gmail.com>
2022-04-03 02:58:48 +0900

    inventory initial set up

---
+++ inventory/main.py
@@ -0,0 +1,60 @@
+from webbrowser import get
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+redis = get_redis_connection(
+    host='redis-13473.c290.ap-northeast-1-2.ec2.cloud.redislabs.com',
+    port=13473,
+    password='0nHUeUqhmUeoPIL7ODCA5NlrgR8v0Jbt',
+    decode_responses=True
+)
+
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+
+@app.get('/products')
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
+
+
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
